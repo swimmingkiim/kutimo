15d53abb87cd303a86ea95a269be2b3ebcd9b927
swimmingkiim <swimmingkiim@gmail.com>
2022-02-01 20:46:10 +0900

    feature : html template literal interpolation eslint

--- example/index.ts
+++ example/index.ts
@@ -6,24 +6,23 @@ const test = "this is test";
 
 class SubElement extends WCSBaseElement {
     static tagName = "sub-element";
+
     constructor() {
         super(SubElement.tagName);
         this.props = {
             name: "default",
-        }
+        };
         this.import = [];
         this.init();
-        this.html`
-            <p>{{this.props.name}}</p>
-        `;
+        this.html`<p>{{ this.props.name }}</p>`;
         this.startRender();
     }
 }
 
-
 class AppElement extends WCSBaseElement {
     static tagName = "wcs-app";
     testValue = 5;
+
     constructor() {
         super(AppElement.tagName);
         this.import = [SubElement];
@@ -33,15 +32,17 @@ class AppElement extends WCSBaseElement {
             name: "swimmingkiim",
         });
         this.html`
-        <div>
-            <p>${test}</p>
-            <p count="{{this.state.count}}">{{this.state.count}}</p>
-            <button on:click="${this.onClickDecrementButton}">decrement</button>
-            <button on:click="${this.onClickIncrementButton}">increment</button>
-            {{
-                this.state.count > this.state.limit ? '<sub-element name="swimmingkiim"></sub-element>' : '<sub-element name="more"></sub-element>'
-            }}
-        </div>`
+      <div>
+        <p>${ test }</p>
+        <p count="{{ this.state.count }}">{{ this.state.count }}</p>
+        <button on:click=" this.onClickDecrementButton }">decrement</button>
+        <button on:click=" this.onClickIncrementButton }">increment</button>
+        {{ 3 > 5 ? '
+        <sub-element name="swimmingkiim"></sub-element>
+        ' : '
+        <sub-element name="more"></sub-element>
+        ' }}
+      </div>`;
         this.startRender();
     }
 
@@ -49,27 +50,28 @@ class AppElement extends WCSBaseElement {
         if (this.state.count === 5 && this.state.limit < 10) {
             this.setState({
                 limit: 10,
-            })
+            });
         }
     }
 
     onAfterRender(): void {
-        console.log("after app(state): ", this.state)
+        console.log("after app(state): ", this.state);
     }
 
     onClickIncrementButton() {
         this.setState({
             count: this.state.count + 1,
-        })
+        });
     }
+
     onClickDecrementButton() {
         this.setState({
             count: this.state.count - 1,
-        })
+        });
     }
 }
 
-WCSBaseElement.registerTag(AppElement)
+WCSBaseElement.registerTag(AppElement);
 
 const container = document.createElement("div");
 
@@ -77,5 +79,3 @@ container.innerHTML = "<wcs-app></wcs-app>";
 
 document.body.querySelector("#wcs-root").innerHTML = "<wcs-app></wcs-app>";
 document.body.querySelector("#wcs-root").appendChild(container);
-
-
